c54257c8f0ddcad9c8685c50c675c14cd7013562
gaurangsaini01 <gaurangsaini01@gmail.com>
2024-03-26 00:32:10 +0530

    Name of App/ Header Changed

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
     <div className="w-full bg-richblack-800 flex items-center justify-center sticky z-50 top-0 scroll" >
       <NavLink to="/">
         <div onClick={()=>window.scroll(0,0)} className="w-full  flex items-center justify-center letter tracking-[0.2rem] text-2xl md:text-3xl font-bold py-2 md:py-2 text-richblack-5">
-          PikaShow
+        CineView🍿📺
         </div>
       </NavLink>
     </div>
